fe94b59bc3755512ef002d314138370697e43bc5
Petr Vesely <petr.vesely@skolakrizik.cz>
2026-01-07 17:53:20 +0100

    Sixth commit, I wonder why it's crashing so much

--- controller/controller_user.php
+++ controller/controller_user.php
@@ -1,13 +1,33 @@
 <?php
 require_once (dirname(__DIR__) ."/model/user.php");
 
-if (!empty($_POST["usr"])||!empty($_POST["pswd"])||!empty($_POST["pswdvrf"])){
+
+if (!empty($_POST["usr"])&&!empty($_POST["pswd"])&&!empty($_POST["pswdvrf"])){
     $usr=trim($_POST["usr"]);
     $pswd=trim($_POST["pswd"]);
     $pswdvrf=trim($_POST["pswdvrf"]);
+
     $registered= User::registered($usr);
     if($registered){
-        echo "mlem";
+
+        $data = User::get_user_data($usr);
+        if($data->rowCount()> 0){
+
+        if($pswd == $data["password"]){
+
+            $_SESSION["user_id"] = $data["id"];
+            echo $data['username'];
+            exit();
+
+        } else{
+            header("Location: /index.php?logging_in=true");
+            exit;
+        }
+    } else {
+        header("Location: /index.php?logging_in=true");
+            exit;
+    }
+
     }else{
         echo "Mium";
     }
--- model/user.php
+++ model/user.php
@@ -1,19 +1,21 @@
 <?php
-
+require "conn.php";
 class User{
 
     public static function registered($username){
-        require_once "conn.php";
-        $sql = "SELECT id FROM user WHERE username=:username"; // Better to select specific column
+        $sql = "SELECT * FROM user WHERE username=:username"; // Better to select specific column
         $qry = $con->prepare($sql);
-        $qry->bindParam(":username", $usernamem, PDO::PARAM_STR);
+        $qry->bindParam(":username", $username, PDO::PARAM_STR);
         $qry->execute();
 
-        if($qry->rowCount() > 0){
-            return true;
-        }else{
-            return false;
-        }
+        return (bool)$qry->fetchColumn();
+    }
 
+    public static function get_user_data($username){
+        $sql = "SELECT * FROM user WHERE username = :username LIMIT 1";
+        $qry = $con->prepare($sql);
+        $qry->bindParam(":username", $username, PDO::PARAM_STR);
+        $qry->execute();
+        return $qry->fetch(PDO::FETCH_ASSOC);
     }
 }
