5bda3ec58e2191ffc7dc941bbea975ad46f82c40
Petr Vesely <petr.vesely@skolakrizik.cz>
2026-01-07 12:13:40 +0100

    Fifth commit it don't work still

--- controller/controller_user.php
+++ controller/controller_user.php
@@ -1,13 +1,4 @@
 <?php
 require_once "model/user.php";
 
-if (!empty($_POST["usr"])||!empty($_POST["pswd"])||!empty($_POST["pswdvrf"])){
-    $usr=trim($_POST["usr"]);
-    $pswd=trim($_POST["pswd"]);
-    $pswdvrf=trim($_POST["pswdvrf"]);
-    $registered=User::get($usr);
-    echo $registered;
-} else {
-    header("Location: /index.php?logging_in=true");
-    exit;
-}
+echo "mlem";
